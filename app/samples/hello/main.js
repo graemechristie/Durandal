2ce587a3db8d2c56b0613851c314fe7fe20e4bfd
Graeme Christie <graeme@sx.net.au>
2012-09-04 19:23:19 +0800

    Some changes to sample structure

---
+++ app/samples/hello/main.js
@@ -0,0 +1,15 @@
+﻿require.config({
+    paths: {
+        "text": "lib/text"
+    }
+});
+
+define(function (require) {
+    var app = require('durandal/app');
+
+    app.start().then(function () {
+        app.makeFit();
+        debugger;
+        app.setRoot('samples/hello/shell', 'applicationHost');
+    });
+});
