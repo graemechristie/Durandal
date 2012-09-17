515e95f6d6e252ac47c61e9ccf362971c8520ecc
Graeme Christie <graeme@sx.net.au>
2012-09-18 00:03:55 +0800

    Fixed typo in chnage summary shim

--- app/lib/change_summary_shim.js
+++ app/lib/change_summary_shim.js
@@ -1,5 +1,7 @@
 ﻿define(function () {
     throw "Currently only browsers that provide ES6 Object.observe as supported. Right now, this means the build of chromium avaliable from here: "
         + "https://github.com/rafaelw/v8"
-        + ". Be sure to enable experimental javascript features in about:config";
+        + ". Be sure to enable experimental javascript features in chrome:flags";
+
+    return null;
 });
